ee42b204f027f169d30db067694eb20fd3badf2f
Mithila Akter <mithila.sm03@gmailcom>
2026-07-31 03:41:10 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,24 @@
+// Problem 1 — Match Winner
+
+function matchWinner(teamAGoals, teamBGoals) {
+  
+    if(typeof teamAGoals !== "number" || typeof teamBGoals !== "number"){
+
+        return "Invalid" ;
+
+    }
+
+    if( teamAGoals > teamBGoals){
+        return "Team A Won" ;
+    }
+
+    else if( teamBGoals > teamAGoals){
+        return "Team B Won" ;
+    }
+    else{
+        return "Draw" ;}
+    
+}
+
+let winner = matchWinner(7,7);
+console.log(winner) ;
